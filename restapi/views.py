59c80f183af5805aa054b31b100e226636f7356d
anubhab-ti <anubhab.das@trilogy.com>
2022-06-02 19:20:18 +0530

    Removed unused and wildcard imports

--- restapi/views.py
+++ restapi/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from decimal import Decimal
-import pandas as pd
-import numpy as np
 import urllib.request
 from datetime import datetime
 
@@ -11,14 +9,13 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import *
+from rest_framework.decorators import api_view, action, authentication_classes, permission_classes
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-from restapi.models import *
-from restapi.serializers import *
-from restapi.custom_exception import *
+from restapi.serializers import Expenses, UserSerializer, Category, CategorySerializer, Groups, GroupSerializer, ExpensesSerializer, UserExpense
+from restapi.custom_exception import UnauthorizedUserException
 from django.db.models.query import QuerySet
 
 
